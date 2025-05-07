d3d260466ac41e6301472d0b19587b422cbd9c34
Yeshwanth3004 <140389065+Yeshwanth3004@users.noreply.github.com>
2025-05-07 10:52:32 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,      // explicitly set dev server port to 3000
+    strictPort: true // fail if port 3000 is already in use
+  },
 });
